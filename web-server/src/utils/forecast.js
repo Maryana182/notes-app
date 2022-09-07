80f8c177a75e0f5b81e06ed92ce0681d541ade65
Maryana182 <111122257+Maryana182@users.noreply.github.com>
2022-09-07 12:34:36 +0300

    init

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -14,8 +14,8 @@ request({url, json: true}, (error, { body }) => {
         
         callback('Unable to find location!',undefined)
     }else {
-        
-        callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out.It feels like ' + body.current.feelslike +  ' degrees out.And the humidity is ' + body.current.humidity)
+
+        callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out.It feels like ' + body.current.feelslike +  ' degrees out. The humidity is ' + body.current.humidity + '%')
     }
 })
 }
