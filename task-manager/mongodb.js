76d69ba821e5ac568a97c4ad96633db3959bed6b
Maryana182 <111122257+Maryana182@users.noreply.github.com>
2022-09-07 21:08:58 +0300

    init

---
+++ task-manager/mongodb.js
@@ -0,0 +1,35 @@
+// CRUD create read update delete
+
+const { MongoClient, ObjectID } = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    const db = client.db(databaseName)
+
+    //  db.collection('users').findOne({ _id: new ObjectID("6318ca330018d4f1d0aaa65b")}, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch')
+    //     }
+
+    //     console.log(user)
+    // })
+
+    //  db.collection('users').find({ age: 27 }).toArray((error, users) => {
+    //     console.log(users)
+    // })
+
+    db.collection('tasks').findOne({ _id: new ObjectID("6318d190ddbd08a4ce3bb0e1") }, (error, task) => {
+        console.log(task)
+    })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks)
+    })
+})
