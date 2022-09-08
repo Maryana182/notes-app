7f04964e779efd941eb4ff049d65c1c9ce978b7f
Maryana182 <111122257+Maryana182@users.noreply.github.com>
2022-09-08 12:49:46 +0300

    init

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,6 +13,67 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
+    // db.collection('users').deleteOne({
+    //     age: 28
+    // }).then((result) => {
+    //      console.log(result)
+    // }).catch((e) => {
+    //     console.log(error)
+    // })
+
+    // db.collection('tasks').deleteMany({
+    //     description: 'Renew inspection'
+    // }).then((result) => {
+    //      console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })  
+
+    // db.collection('tasks').updateMany({
+    //     completed: false
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }).then((result) => {
+    //     console.log(result.modifiedCount)
+    // }).catch((error) => {
+    //     console.log(error.modifiedCount)
+    // })
+
+    
+    // const updatePromise = db.collection('users').updateOne({
+    //     _id : new ObjectID("6318c7e42ca305bb4199ebb6")
+    // }, {
+    //     $inc: {
+    //         age: -29
+    //     }
+    // })
+
+    // updatePromise.then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    // const updatePromise = db.collection('users').updateOne({
+    //     _id : new ObjectID("6318c7e42ca305bb4199ebb6")
+    // }, {
+    //     $set: {
+    //         name: 'Mike'
+    //     }
+    // })
+
+    // updatePromise.then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    // const id = new ObjectID()
+    // console.log(id.id.length)
+    // console.log(id.toHexString().length)
+
     //  db.collection('users').findOne({ _id: new ObjectID("6318ca330018d4f1d0aaa65b")}, (error, user) => {
     //     if (error) {
     //         return console.log('Unable to fetch')
@@ -25,11 +86,11 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     //     console.log(users)
     // })
 
-    db.collection('tasks').findOne({ _id: new ObjectID("6318d190ddbd08a4ce3bb0e1") }, (error, task) => {
-        console.log(task)
-    })
+//     db.collection('tasks').findOne({ _id: new ObjectID("6318d190ddbd08a4ce3bb0e1") }, (error, task) => {
+//         console.log(task)
+//     })
 
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        console.log(tasks)
-    })
+//     db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+//         console.log(tasks)
+//     })
 })
